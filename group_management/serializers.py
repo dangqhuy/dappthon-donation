3543d3a0d8fe11fd12d051cf706a9404764f54d2
tailouis <vuductaiptit@gmail.com>
2018-09-30 06:34:06 +0700

    update Groups api

--- group_management/serializers.py
+++ group_management/serializers.py
@@ -5,11 +5,22 @@ from django.db.models import Count
 from . import models
 
 
+class EventSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Event
+        exclude=['created', 'modified', 'is_removed']
+
+
 class GroupSerializer(serializers.ModelSerializer):
-    
+    events = EventSerializer(read_only=True, many=True)
+    events_count = serializers.SerializerMethodField()
+
 
     class Meta:
         model = models.Group
-        fields = '__all__'
+        exclude=['created', 'modified', 'is_removed']
 
+    def get_events_count(self, model):
+        return model.events.count()
 
