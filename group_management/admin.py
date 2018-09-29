fc759c905e8dc85f04f9c9a37e57c91014a05ea4
dangqhuy <dangqhuy2411@gmail.com>
2018-09-29 23:12:13 +0700

    done api get list of group

--- group_management/admin.py
+++ group_management/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+class EventInline(admin.TabularInline):
+    model = models.Event
+
+
+class GroupAdmin(admin.ModelAdmin):
+    inlines = [
+        EventInline,
+    ]
+
+class ActivityInline(admin.TabularInline):
+    model = models.Activity
+
+class EventAdmin(admin.ModelAdmin):
+    inlines = [
+        ActivityInline,
+    ]
+
+admin.site.register(models.Profile)
+admin.site.register(models.Group, GroupAdmin)
+admin.site.register(models.Event, EventAdmin)
+admin.site.register(models.Activity)
