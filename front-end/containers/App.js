e577cca6b40f56610ab92abbcb3b61689e79699f
Nam <trantrungnam1997vn2@gmail.com>
2018-09-29 21:28:07 +0700

    Update front-end

--- front-end/containers/App.js
+++ front-end/containers/App.js
@@ -1,19 +1,25 @@
-import * as React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import Header from 'Header'
-import Menu from 'Menu'
 
+import * as React from 'react'
+import Header from './Header'
+import Menu from './Menu'
+import HomePage from './HomePage'
+import Events from './Events'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 class App extends React.Component {
-
     render() {
         return (
-            <div>
-                <Header></Header>
-                <Menu></Menu>
-                <Router>
-                    <Route path="/" component={HomePage}></Route>
-                </Router>
+            <div className="page">
+                <div className="page-main">
+                    <Header />
+                    <Menu />
+                    <BrowserRouter>
+                        <div>
+                            <Route exact path="/" component={HomePage}></Route>
+                            <Route path="/events/:id" component={Events}></Route>
+                        </div>
+                    </BrowserRouter>
+                </div>
             </div>
         )
     }
---
+++ front-end/containers/Events.js
@@ -0,0 +1,62 @@
+import * as React from 'react'
+
+
+class Events extends React.Component {
+
+
+  render() {
+    return (
+      <div className="my-3 my-md-5">
+        <div className="container">
+          <div className="row row-cards row-deck">
+            <div className="col-12">
+              <div className="card">
+                <div className="card-header">
+                  <h3 className="card-title">Events</h3>
+                </div>
+                <div className="table-responsive">
+                  <table className="table card-table table-vcenter text-nowrap">
+                    <thead>
+                      <tr>
+                        <th className="w-1">No.</th>
+                        <th>Name</th>
+                        <th>Company</th>
+                        <th>Created</th>
+                        <th>Expire</th>
+                        <th>Status</th>
+                        <th>Target</th>
+                        <th>Balance</th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td><span className="text-muted">001401</span></td>
+                        <td><a href="invoice.html" className="text-inherit">Design Works</a></td>
+                        <td>
+                          Carlson Limited</td>
+                        <td>
+                          29 Sep 2018</td>
+                        <td>
+                          29 October 2018</td>
+                        <td>
+                          <span className="status-icon bg-success"></span> Paid</td>
+                        <td>$887</td>
+                        <td>$534</td>
+                        <td className="text-right">
+                          <a href="#" className="btn btn-secondary btn-sm">Donate</a>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Events;
